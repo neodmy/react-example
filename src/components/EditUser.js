917cba7973f1fc408e74cef6abe0951f0e5d5f38
David Miguel Yusta <36865163+neodmy@users.noreply.github.com>
2020-08-24 23:18:38 +0200

    feat: add basic structure for EditUser component (#3)

--- src/components/EditUser.js
+++ src/components/EditUser.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+} from 'reactstrap';
 
 export const EditUser = () => {
   return (
-    <div>
-     <h1>EditUser</h1>
-    </div>
+    <Form>
+      <FormGroup>
+        <Label>Name</Label>
+        <Input type='text' placeholder='Enter Name'></Input>
+      </FormGroup>
+      <Button type='submit'>Edit Name</Button>
+      <Link to='/' className='btn btn-danger ml-2'>Cancel</Link>
+    </Form>
   );
 }
